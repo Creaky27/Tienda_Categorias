58f868b99e27e0c1ebe323ea5a52e0202c351302
creaky27 <moana.padilla.0@gmail.com>
2022-07-27 21:05:45 -0500

    Cambio y eliminación ".php"

--- funciones.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("connection.php");
-
-function get_all_categories($connect){
-    $consulta = "SELECT * FROM categories";
-    $resultado = mysqli_query($connect,$consulta);
-    return $resultado;
-}
-?>
