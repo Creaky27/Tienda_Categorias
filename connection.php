9b04fe39f281a9e20250ad3f5c95aea22427b261
creaky27 <moana.padilla.0@gmail.com>
2022-08-02 19:10:31 -0500

    Eliminación documentos repetidos

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $db = "elorigin_unid";
-    $user = "root";
-    $password = "";
-    $host = "localhost";
-
-    $connect = mysqli_connect($host, $user, $password, $db);
-?>
--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("funtions.php")
-
-$id = $_GET['id'];
-
-delete_categorie($id);
-
-header("Location: categorias.php");
-?>
