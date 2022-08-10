fd8e393f84c2d3b2619cfcebd6498204210ce645
creaky27 <moana.padilla.0@gmail.com>
2022-08-09 19:14:56 -0500

    Documentos perdidos

--- CATEGORIA/categorias.php
+++ CATEGORIA/categorias.php
@@ -1,13 +1,7 @@
 <?php
 require_once("../LIB/functions.php");
 $categoria = get_all_categories($connect);
-
-$mysqli = NEW MySQLi('localhost', 'root', '', 'elorigin_unid');
-
-
-
 ?>
-
 <!DOCTYPE html>
 <html lang="es-MX">
 <head>
@@ -20,59 +14,29 @@ $mysqli = NEW MySQLi('localhost', 'root', '', 'elorigin_unid');
     <body>
     <h1>Categorias</h1>
     <h4><small><a href="insert.php">Agregar Categoria</a></small></h4>
-       
-        <?php
-        
-                if(isset($_GET['order'])){
-                $order = $_GET['order'];
-                }else{
-                        $order = 'name';
+        <table border="2">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    while ($fila = mysqli_fetch_array($categoria)){
+                ?>
+                <tr>
+                    <td><?php echo $fila['id'];?></td>
+                    <td><?php echo $fila['name'];?></td>
+                    <td> <a href= "producto.php?id=<?php echo $fila ['id']?>">Productos</a></td>
+                    <td> <a href= "update.php?id=<?php echo $fila ['id']?>">Editar</a></td>
+                    <td> <a href= "delete.php?id=<?php echo $fila['id']?>">Eliminar</a></td>
+                </tr>
+                <?php
                 }
-                if(isset($_GET['sort'])){
-                        $sort = $_GET['sort'];
-                }else{
-                        $sort = 'ASC' ;
-                }
-                
-                $resultSet = $mysqli->query("SELECT * FROM categories ORDER BY $order $sort");
-
-
-    	        if($resultSet->num_rows > 0){
-                        $sort == 'DESC' ? $sort = 'ASC' : $sort = 'DESC' ;
-
-                        echo"
-                        
-                        <table border='2'>
-                        <thead>
-                                <tr>
-                                <th><a href='?order=id&&sort=$sort'>ID↑↓</a></th>
-                                <th><a href='?order=name&&sort=$sort'>Nombre↑↓</a></th>
-                                </tr>
-                        </thead>
-                        ";
-                        while($rows = $resultSet->fetch_assoc())
-                        {
-                                $asset_num = $rows['id'];
-                                $sort = $rows['name'];
-                                $getID = $rows['id'] ;
-                                
-                                echo"
-                                        <tr>
-                                        <td> $asset_num </td>
-                                        <td> $sort </td>
-                                        <td> <a href= '#'>Productos</a></td>
-                                        <td> <a href= 'update.php?id=$getID'>Editar</a></td>
-                                        <td> <a href= 'delete.php?id=$getID'>Eliminar</a></td>
-                                        </tr>
-                                ";
-                        }
-                        echo "
-                        </table>
-                        ";
-                        }else{
-                                echo "No records returnes.";
-                        }
-        ?>
-</body>
+                ?>
+            </tbody>
+        </table>
+    </body>
 </center>
 </html>
--- CATEGORIA/delete.php
+++ CATEGORIA/delete.php
@@ -1,5 +1,5 @@
 <?php
-require_once("../lib/functions.php") ;
+require_once("functions.php") ;
 
 $id = $_GET['id'];
 
