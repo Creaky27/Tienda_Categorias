2d1b4d9fd13db0ef5e186c1c22c160ea6e231889
kleecollage <56179611+kleecollage@users.noreply.github.com>
2022-08-15 09:50:54 -0500

    Se agrega la opcion status y se crea funcion
    Se agrega la función que muestra las tablas de categorias en lib/functions
    Adicional se muestra el status como "disponible" o "agotado"

--- CATEGORIA/categorias.php
+++ CATEGORIA/categorias.php
@@ -1,7 +1,6 @@
 <?php
 require_once("../LIB/functions.php");
-$categoria = get_all_categories($connect);
-$mysqli = NEW MySQLi('localhost', 'root', '', 'elorigin_unid');
+$name = $_POST["user"] ;
 ?>
 
 <!DOCTYPE html>
@@ -14,8 +13,22 @@ $mysqli = NEW MySQLi('localhost', 'root', '', 'elorigin_unid');
 </head>
 <center>
     <body>
-    <h1>Categorias</h1><h4><small><a href="#">Regresar</a></small></h4>
-    <h4><small><a href="insert.php">Agregar Categoria</a></small></h4>
+
+        <h1>Todas las Categorías</h1>
+        <form id='users_id' action="envio.php" method='POST'></form>
+        <select name="user" id="user">
+        <option value = "0"> Seleccionar Cliente </option>
+        <?php
+                
+                $user = "SELECT * FROM users" ;
+                $resultado = mysqli_query($connect, $user) ;
+                /*  BUSCADOR POR IMPLEMENTAR
+                        while ($valores = mysqli_fetch_array($resultado)) {
+                        echo '<option value = "'.$valores[names].'">'.$valores[names].'</option>';
+                } */  
+        ?>
+        </select>
+
         <?php
         
                 if(isset($_GET['order'])){
@@ -28,46 +41,13 @@ $mysqli = NEW MySQLi('localhost', 'root', '', 'elorigin_unid');
                 }else{
                         $sort = 'ASC' ;
                 }
-                
-                $resultSet = $mysqli->query("SELECT * FROM categories ORDER BY $order $sort");
 
-
-    	        if($resultSet->num_rows > 0){
-                        $sort == 'DESC' ? $sort = 'ASC' : $sort = 'DESC' ;
-
-                        echo"
-                        
-                        <table border='2'>
-                        <thead>
-                                <tr>
-                                <th><a href='?order=id&&sort=$sort'>ID↑↓</a></th>
-                                <th><a href='?order=name&&sort=$sort'>Nombre↑↓</a></th>
-                                </tr>
-                        </thead>
-                        ";
-                        while($rows = $resultSet->fetch_assoc())
-                        {
-                                $asset_num = $rows['id'];
-                                $sort = $rows['name'];
-                                $getID = $rows['id'] ;
-                                
-                                echo"
-                                        <tr>
-                                        <td> $asset_num </td>
-                                        <td> $sort </td>
-                                        <td> <a href= '#'>Productos</a></td>
-                                        <td> <a href= 'update.php?id=$getID'>Editar</a></td>
-                                        <td> <a href= 'delete.php?id=$getID'>Eliminar</a></td>
-                                        </tr>
-                                ";
-                        }
-                        echo "
-                        </table>
-                        ";
-                }else{
-                        echo "No records returnes.";
-                }
+    	       categorie_table($order , $sort);
         ?>
+
+        <h4><small><a href="insert.php">Agregar Categoria</a></small></h4>
+        <h4><small><a href="#">Regresar</a></small></h4>
+
         </body>
 </center>
 </html>
--- LIB/functions.php
+++ LIB/functions.php
@@ -1,6 +1,54 @@
 <?php
 require_once("connection.php");
 
+function categorie_table($order , $sort){
+    $mysqli = NEW MySQLi('localhost', 'root', '', 'elorigin_unid');
+    $resultSet = $mysqli->query("SELECT * FROM categories ORDER BY $order $sort");
+
+    if($resultSet->num_rows > 0){
+        $sort == 'DESC' ? $sort = 'ASC' : $sort = 'DESC' ;
+
+        echo"
+        
+        <table border='2'>
+        <thead>
+                <tr>
+                <th><a href='?order=id&&sort=$sort'>ID↑↓</a></th>
+                <th><a href='?order=name&&sort=$sort'>Nombre↑↓</a></th>
+                <th> Status </th>
+                </tr>
+        </thead>
+        ";
+        while($rows = $resultSet->fetch_assoc())
+        {
+                $getID = $rows['id'];
+                $sort = $rows['name'];
+                $getStatus = $rows['status'];
+                if($getStatus == 1){
+                        $getStatus = 'Disponible' ;
+                } else {
+                        $getStatus = 'Agotado' ;
+                }
+                
+                
+                echo"
+                        <tr>
+                        <td> $getID </td>
+                        <td> <a href= '#'> $sort </a></td>
+                        <td> $getStatus </td>
+                        <td> <a href= 'update.php?id=$getID'>Editar</a></td>
+                        <td> <a href= 'delete.php?id=$getID'>Eliminar</a></td>
+                        </tr>
+                ";
+        }
+        echo "
+        </table>
+        ";
+    }else{
+        echo "No records returnes.";
+    }
+}
+
 function get_all_categories($connect){
     $consulta = "SELECT * FROM categories";
     $resultado = mysqli_query($connect,$consulta);
